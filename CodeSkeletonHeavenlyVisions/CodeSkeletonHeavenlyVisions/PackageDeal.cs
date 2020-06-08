c42dd9cc5a02105cfbe41e54be6ed7f9910c4fa9
Seymour <reevs3@student.op.ac.nz>
2020-06-08 15:31:14 +1200

    Seymour

---
+++ CodeSkeletonHeavenlyVisions/CodeSkeletonHeavenlyVisions/PackageDeal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSkeletonHeavenlyVisions
+{
+    class PackageDeal
+    {
+        public double dealPric;
+        public int dealDiscount;
+        public string expiryDate;
+        public List<Service>;
+
+
+
+
+    }
+}
---
+++ CodeSkeletonHeavenlyVisions/CodeSkeletonHeavenlyVisions/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSkeletonHeavenlyVisions
+{
+    class Service
+    {
+        public bool foodintolernet;
+        public avalabileRooms List<int>;
+        mealPackages List<string>;
+        public string requiredEqiupment;
+        public string pickUpLocation;
+        public bool LugggaeAgreementAccepted;
+
+
+
+
+
+        public void Service()
+        { }
+
+
+
+    }
+}
