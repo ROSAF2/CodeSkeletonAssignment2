749ea4d88f026264fba796f80ff26594b233e69b
Rudy-5898 <Realdi1@student.op.ac.nz>
2020-06-08 15:51:32 +1200

    added code

---
+++ CodeSkeletonHeavenlyVisions/CodeSkeletonHeavenlyVisions/Website.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSkeletonHeavenlyVisions
+{
+    class Website
+    {
+        private List<Booking>;
+        private string emailList;
+        private string waitingList;
+        private string regularCustomerList;
+
+        public Website()
+        {
+            new EmailObject();
+        }
+        public void DisplayContent()
+        { }
+        public void UseEmailObject()
+        { }
+        public void VerifyCredentials()
+        { }
+        public void GrantAccessToUserAccount()
+        { }
+        public void AskToGenerateInvoice()
+        { }
+        public void GenerateInvoice()
+        { }
+        public void AskToJoinWaitingList()
+        { }
+        public void AppendWaitingList()
+        { }
+        public void ChargeFullBookings()
+        { }
+        public void ChargeTenPercent()
+        { }
+        public void RemoveFromWaitingList()
+        { }
+        public void AppendToBookingList()
+        { }
+        public void RemoveFromBookingList()
+        { }
+        public void Refund()
+        { }
+        public void VerifySpaceInBookingList()
+        { }
+        public void ProduceReport()
+        { }
+
+
+
+
+    }
+}
